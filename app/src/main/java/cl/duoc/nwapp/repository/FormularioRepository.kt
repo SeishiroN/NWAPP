530f8e554898907ce12b054b040f849219e357fb
MrJeff22 <je.carbonell@duocuc.cl>
2025-10-21 19:38:16 -0300

    Comentarios iniciales (FormularioRepository)

--- app/src/main/java/cl/duoc/nwapp/repository/FormularioRepository.kt
+++ app/src/main/java/cl/duoc/nwapp/repository/FormularioRepository.kt
@@ -3,48 +3,53 @@ package cl.duoc.nwapp.repository
 import cl.duoc.nwapp.model.FormularioModel
 import cl.duoc.nwapp.model.MensajesError
 
+class FormularioRepository {
+    private var formulario = FormularioModel() // Esto trae el formulario model de ingreso, guardandose en la variable formulario
+    private var errores = MensajesError() // Esto trae el formulario model del error en el ingreso, guardandose en la variable
 
-class  FormularioRepository {
+    fun getFormulario(): FormularioModel = formulario // Trae el formulario
 
-    private var formulario = FormularioModel()
-    private var errores = MensajesError()
+    fun getMensajesError(): MensajesError = errores // Trae los errores
 
-    fun getFormulario():  FormularioModel = formulario
-    fun getMensajesError():  MensajesError = errores
-
-
+    //
     fun cambiarNombre(nuevoNombre: String) {
         formulario.nombre = nuevoNombre
     }
 
+    // Para validar el nombre (En este caso, forzaremos el programa a que solo acepte ADMIN)
     fun validacionNombre(): Boolean {
-        if(formulario.nombre!="ADMIN")
+        if (formulario.nombre != "ADMIN") {
             return false
-        else
+        } else {
             return true
+        }
     }
 
+    // Valida que en el formulario, haya ingresado un string equivalente al patrón indicado en el formulario
     fun validacionCorreo(): Boolean {
-        if (!formulario.correo.matches(Regex("^[\\w.-]+@[\\w.-]+\\.\\w+$")))
+        if (!formulario.correo.matches(Regex("^[\\w.-]+@[\\w.-]+\\.\\w+$"))) {
             return false
-        else
+        } else {
             return true
+        }
     }
 
+    // Valida que la edad ingresada sea mayor a 18 (años)
     fun validacionEdad(): Boolean {
         val edadInt = formulario.edad.toIntOrNull()
-        if (edadInt == null || edadInt < 0 || edadInt > 120)
+        if (edadInt == null || edadInt < 0 || edadInt > 120) {
             return false
-        else
+        } else {
             return true
+        }
     }
 
+    // Valida que la casilla de "Términos" haya sido selccionada.
     fun validacionTerminos(): Boolean {
-        if (!formulario.terminos)
+        if (!formulario.terminos) {
             return false
-        else
+        } else {
             return true
+        }
     }
-
-
 }
